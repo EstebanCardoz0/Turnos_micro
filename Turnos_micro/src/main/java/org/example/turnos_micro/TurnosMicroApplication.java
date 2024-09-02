19309d8c1a9be3c453fdc3ca8c8309954dfb2472
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2024-09-01 22:43:00 -0300

    1 sep

---
+++ Turnos_micro/src/main/java/org/example/turnos_micro/TurnosMicroApplication.java
@@ -0,0 +1,13 @@
+package org.example.turnos_micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurnosMicroApplication.class, args);
+    }
+
+}
